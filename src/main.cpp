245110269a0461ea52ebf7febe494d90b527af15
fmorac <fabian.mcg@crpl.udel.edu>
2019-10-21 09:37:56 -0400

    Makefile added

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,12 @@ using namespace __core__;
 typedef double real_T;
 int main(int argc,char *argv[]) {
 	bool print=false;
+	bool check_inverse=false;
 	bool random=true;
     cerr<<std::fixed;
     cerr<<std::setprecision(9);
     double ct=0,bt=0,ft=0;
-    size_t n=40;
+    size_t n=400;
     size_t nr=1;
     Matrix<real_T> A;
     if(argc>1) {
@@ -60,30 +61,49 @@ int main(int argc,char *argv[]) {
     	ct+=timer.elapsed_time();
     }
     if(print) cerr<<U<<endl<<endl;
-    Matrix<real_T> I=identity<real_T>(n);
-    Matrix<real_T> L=U.transpose();
-    Matrix<real_T> LI;
-    for(size_t k=0;k<nr;++k) {
-    	timer.start();
-    	LI=backward(L,I);
-    	timer.stop();
-    	bt+=timer.elapsed_time();
+    if(check_inverse) {
+    	Matrix<real_T> I=identity<real_T>(n);
+    	Matrix<real_T> L=U.transpose();
+    	Matrix<real_T> LI;
+    	for(size_t k=0;k<nr;++k) {
+    		timer.start();
+    		LI=backward(L,I);
+    		timer.stop();
+    		bt+=timer.elapsed_time();
+    	}
+    	Matrix<real_T> AI;
+    	for(size_t k=0;k<nr;++k) {
+    		timer.start();
+    		AI=forward(U,LI);
+    		timer.stop();
+    		ft+=timer.elapsed_time();
+    	}
+    	if(print) cerr<<AI<<endl<<endl;
+    	Matrix<real_T> IA=A*AI;
+    	if(print) cerr<<IA<<endl<<endl;
+    	cerr<<"************************************************************************************************"<<endl<<endl;
+    	cerr<<"Cholesky decomposition timing:\t"<<ct/nr<<endl;
+    	cerr<<"Backward solving timing:\t"<<bt/nr<<endl;
+    	cerr<<"Forward solving timing:\t"<<ft/nr<<endl;
+    	//    cerr<<"Error:\t"<<IA.distance_max(IA,I)<<endl<<endl;
+    	cerr<<endl<<"************************************************************************************************"<<endl;
     }
-    Matrix<real_T> AI;
-    for(size_t k=0;k<nr;++k) {
-    	timer.start();
-    	AI=forward(U,LI);
-    	timer.stop();
-    	ft+=timer.elapsed_time();
+    else {
+    	Matrix<real_T> AA=U.transpose();
+    	AA=AA*U;
+    	if(print) cerr<<AA<<endl<<endl;
+    	AA=AA.substract(A);
+    	if(print) cerr<<AA<<endl<<endl;
+    	cerr<<"************************************************************************************************"<<endl<<endl;
+    	cerr<<"Cholesky decomposition timing:\t"<<ct/nr<<endl;
+        cerr<<std::setprecision(16);
+    	cerr<<"Error:\t"<<AA.norm()<<endl;
+    	for(size_t i=0;i<AA.n()*AA.m();++i)
+    		if(isnan(AA[i])){
+    			cerr<<"There is a NaN on the result."<<endl;
+    			break;
+    		}
+    	cerr<<endl<<"************************************************************************************************"<<endl;
     }
-    if(print) cerr<<AI<<endl<<endl;
-    Matrix<real_T> IA=A*AI;
-    if(print) cerr<<IA<<endl<<endl;
-	cerr<<"************************************************************************************************"<<endl<<endl;
-    cerr<<"Cholesky decomposition timing:\t"<<ct/nr<<endl;
-    cerr<<"Backward solving timing:\t"<<bt/nr<<endl;
-    cerr<<"Forward solving timing:\t"<<ft/nr<<endl;
-//    cerr<<"Error:\t"<<IA.distance_max(IA,I)<<endl<<endl;
-	cerr<<endl<<"************************************************************************************************"<<endl;
 	return 0;
 }
